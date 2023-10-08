118df7075607d6bf1edba1e74c9ba954051bc925
Dorin Crasnojon <dorin.crasnojon@ishunea.io>
2023-10-08 15:01:55 +0300

    Add project files.

---
+++ Broker/Services/SubscriberService.cs
@@ -0,0 +1,43 @@
+﻿using Broker.Models;
+using Broker.Services.Interfaces;
+using Grpc.Core;
+using GrpcAgent;
+using System;
+using System.Threading.Tasks;
+
+namespace Broker.Services
+{
+    public class SubscriberService:Subscriber.SubscriberBase
+    {
+        private readonly IConnectionStorageService _connectionStorage;
+        public SubscriberService(IConnectionStorageService connectionStorage)
+        {
+            _connectionStorage = connectionStorage;    
+        }
+
+        public override Task<SubscribeReply> Subscribe(SubscribeRequest request, ServerCallContext context)
+        {
+            Console.WriteLine($"New client trying to subscribe: {request.Address} {request.Topic}");
+
+            try
+            {
+                var connection = new Connection(request.Address, request.Topic);
+
+                _connectionStorage.Add(connection);
+            }
+
+            catch(Exception ex) {
+                Console.WriteLine($"Could not add the new connection {request.Address} {request.Topic} {ex.Message}");
+            
+            }
+          
+
+            
+            return Task.FromResult(new SubscribeReply()
+            {
+                IsSuccess = true,
+            });
+        }
+
+    }
+}
---
+++ Common/EndpointsConstants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Common
+{
+    public class EndpointsConstants
+    {
+        public const string BrokerAddress = "http://127.0.0.1:5001";
+
+        public const string SubcribersAddress = "http://127.0.0.1:0";
+    }
+}
